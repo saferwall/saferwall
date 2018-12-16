d1307423bfcac5821c9752dbb2ac63c779fe9ad3
LordNoteworthy <lord.noteworthy@protonmail.com>
2018-12-16 08:44:04 -0800

    add somer common utils functions

---
+++ web/server/app/common/utils/utils.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo"
+)
+
+// IsStringInSlice check if a string exist in a list of strings
+func IsStringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+
+	return false
+}
+
+// GetQueryParamsFields retrieve `fields`` so we can filter them in GET/
+func GetQueryParamsFields(c echo.Context) []string {
+	var filters []string
+	fields := c.QueryParam("fields")
+	if fields != "" {
+		filters = strings.Split(fields, ",")
+	}
+
+	return filters
+}
+
+// IsFilterAllowed check if we are allowed to filter GET with fields
+func IsFilterAllowed(allowed []string, filters []string) bool {
+	for _, filter := range filters {
+		if !IsStringInSlice(filter, allowed) {
+			return false
+		}
+	}
+	return true
+}
+
+// JSONTime alias for timne.Time
+type JSONTime time.Time
+
+// MarshalJSON will marshall date
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	//do your serializing here
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("Mon Jan _2"))
+	return []byte(stamp), nil
+}
