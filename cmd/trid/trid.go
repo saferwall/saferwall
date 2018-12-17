bbd5140b9e601f0e685c1a3090d3bd49d5476e20
LordNoteworthy <lord.noteworthy@protonmail.com>
2018-12-17 01:34:53 -0800

    add TriD cmd

---
+++ cmd/trid/trid.go
@@ -0,0 +1,36 @@
+// Copyright 2018 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/saferwall/saferwall/pkg/trid"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: trid <filepath>")
+		return
+	}
+
+	// get exiftool output
+	r, err := trid.Scan(os.Args[1])
+	check(err)
+
+	// pretty print the results
+	b, err := json.MarshalIndent(r, "", "  ")
+	check(err)
+	os.Stdout.Write(b)
+}
