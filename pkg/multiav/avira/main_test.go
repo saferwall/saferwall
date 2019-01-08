4c9f92dbaeecb5c157bc2168485cab37927300eb
LordNoteworthy <lord.noteworthy@protonmail.com>
2019-01-08 10:51:16 -0800

    [pkg/avira] more on license status check

--- pkg/multiav/avira/main_test.go
+++ pkg/multiav/avira/main_test.go
@@ -77,3 +77,18 @@ func TestGetLicenceStatusNoLicenseFound(t *testing.T) {
 
 	}
 }
+
+func TestGetLicenceStatusExpiredLicense(t *testing.T) {
+
+	// Deliberately copy an expired license
+	err := os.Remove(LicenseKeyPath)
+	if err != nil {
+		t.Errorf("TestIsLicenseExpiredNoLicenseFound failed, err: %s", err)
+	}
+
+	_, err = GetLicenseStatus()
+	if err != ErrNoLicenseFound {
+		t.Errorf("TestIsLicenseExpiredNoLicenseFound failed, err: %s", err)
+
+	}
+}
