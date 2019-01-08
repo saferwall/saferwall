bb9c90793201c2b962e89d770bcc6a22f389fafc
LordNoteworthy <lord.noteworthy@protonmail.com>
2019-01-08 06:05:49 -0800

    [pkg/crypto] add unit test

--- scripts/commit_check.sh
+++ scripts/commit_check.sh
@@ -6,11 +6,10 @@ set -e
 # latest commit
 LATEST_COMMIT=$(git rev-parse HEAD)
 
-# latest commit where path/to/folder1 was changed
+# latest commit where a folder was changed
 PKG_AVAST_COMMIT=$(git log -1 --format=format:%H --full-diff scripts/)
-
-# latest commit where path/to/folder2 was changed
 PKG_AVIRA_COMMIT=$(git log -1 --format=format:%H --full-diff pkg/multiav/avira)
+PKG_CRYPTO_COMMIT=$(git log -1 --format=format:%H --full-diff pkg/crypto)
 
 if [ $PKG_AVAST_COMMIT = $LATEST_COMMIT ];
     then
@@ -18,8 +17,12 @@ if [ $PKG_AVAST_COMMIT = $LATEST_COMMIT ];
         touch /tmp/avast.circleci
 elif [ $PKG_AVIRA_COMMIT = $LATEST_COMMIT ];
     then
-        echo "files in circleci has changed"
+        echo "files in pkg/multiav/avira has changed"
         touch /tmp/avira.circleci
+elif [ $PKG_CRYPTO_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "files in pkg/crypto has changed"
+        touch /tmp/crypto.circleci
 else
      echo "no folders of relevance has changed"
      exit 0;
