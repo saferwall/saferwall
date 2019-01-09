f95e6037916f963d6c2a3ebba1e388c74e9bb8d1
LordNoteworthy <lord.noteworthy@protonmail.com>
2019-01-09 04:01:36 -0800

    [circleci] fix typo

--- pkg/multiav/avast/main.go
+++ pkg/multiav/avast/main.go
@@ -147,7 +147,7 @@ func ActivateLicense(r io.Reader) error {
 		return err
 	}
 
-	// Restart the daemon to apply the license
+	// Restart the service to apply the license
 	err = RestartService()
 	if err != nil {
 		return err
