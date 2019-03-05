d1506e0e23e83c3790c6e3527aa7732b55aab6c1
LordNoteworthy <lord.noteworthy@protonmail.com>
2019-03-05 01:43:53 -0800

    [core/consumer]add strings pkg results to consumer

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -1,4 +1,8 @@
-package string
+// Copyright 2018 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package strings
 
 import (
 	"bytes"
