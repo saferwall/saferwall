e2b0689b7daaffc17c3bd6940eae9287f89c9cd9
LordNoteworthy <lord.noteworthy@protonmail.com>
2019-05-15 07:03:40 +1000

    [website] update jekyll gemfile

--- build/mk/nodejs.mk
+++ build/mk/nodejs.mk
@@ -1,6 +1,6 @@
 nodejs-install:		## Install nodejs
 	curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-	apt-get update
-	apt-get install -y nodejs
+	sudo apt-get update
+	sudo apt-get install -y nodejs
 	node -v
 	npm -v
