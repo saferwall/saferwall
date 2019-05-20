738ee31bd90639fd95248e2ae1f941343d9066a5
Marouane Rassili <mrassili@aol.com>
2019-05-20 00:15:14 +0000

    Fix Webpack production build

--- ui/build/utils.js
+++ ui/build/utils.js
@@ -49,7 +49,7 @@ exports.cssLoaders = function(options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return MiniCssExtractPlugin.loader;
+      return [MiniCssExtractPlugin.loader].concat(loaders);
     } else {
       return ["vue-style-loader"].concat(loaders);
     }
