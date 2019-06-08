412579ce261c40c0d10dc8d71c4cecb013579971
LordNoteworthy <lord.noteworthy@protonmail.com>
2019-06-08 11:44:49 +1000

    [core/multiav] fix symantec gRPC server

--- core/multiav/symantec/main.go
+++ core/multiav/symantec/main.go
@@ -58,16 +58,18 @@ func NewServer(opts ...grpc.ServerOption) *grpc.Server {
 func main() {
 
 	log.Infoln("Starting Symantec daemon `symcfgd`")
-	err := utils.StartCommand("sudo", "/opt/Symantec/symantec_antivirus/symcfgd", "-x")
+	out, err := utils.ExecCommand("sudo", "/opt/Symantec/symantec_antivirus/symcfgd", "-x")
 	if err != nil {
 		log.Errorf("Starting symcfgd failed: %v", err)
 	}
+	log.Infoln(out)
 
 	log.Infoln("Starting Symantec daemon `rtvscand`")
-	err = utils.StartCommand("sudo", "/opt/Symantec/symantec_antivirus/rtvscand", "-x")
+	out, err = utils.ExecCommand("sudo", "/opt/Symantec/symantec_antivirus/rtvscand", "-x")
 	if err != nil {
 		log.Errorf("Starting rtvscand failed: %v", err)
 	}
+	log.Infoln(out)
 
 	log.Infoln("Starting Symantec gRPC server")
 
