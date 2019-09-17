e646e77776456b5397c84447119a57d2c084f3be
Ayoub Faouzi <lord.noteworthy@protonmail.com>
2019-09-18 09:53:18 +1000

    [pkg/utils] fix pkg again

--- pkg/utils/minio.go
+++ pkg/utils/minio.go
@@ -1,26 +1,23 @@
-
 // Copyright 2019 Saferwall. All rights reserved.
 // Use of this source code is governed by Apache v
 // license that can be found in the LICENSE file.
 
 package utils
 
-
 import (
-	"path"
 	"github.com/minio/minio-go/v6"
+	"path"
 )
 
-
 // Download downloads an object from a bucket.
-func Download (client* minio.Client, bucketName string, objectName string) ([]byte, error) {
-	filePath := path.Join("/"+ bucketName, objectName)
+func Download(client *minio.Client, bucketName string, objectName string) ([]byte, error) {
+
+	filePath := path.Join("/"+bucketName, objectName)
 	err := client.FGetObject(bucketName, objectName, filePath, minio.GetObjectOptions{})
 	if err != nil {
-		return []byte, err
+		return nil, err
 	}
 
 	data, err := ReadAll(filePath)
 	return data, err
 }
-
