dd28838bf4a0901dc94abae35ca63691ef470be2
Ayoub Faouzi <lord.noteworthy@protonmail.com>
2019-09-23 15:35:20 +1000

    rename internal to core back

--- build/mk/multiav.mk
+++ build/mk/multiav.mk
@@ -46,7 +46,7 @@ endif
 ifeq ($(AV_VENDOR),eset)
 	$(eval DOCKER_BUILD_ARGS = "--build-arg ESET_USER=$(ESET_USER) --build-arg ESET_PWD=$(ESET_PWD)")
 endif
-	sudo make docker-release ARGS=$(DOCKER_BUILD_ARGS) IMG=$(AV_VENDOR) VERSION=0.0.1 DOCKER_FILE=build/docker/Dockerfile.$(AV_VENDOR) DOCKER_DIR=build/docker
+	sudo make docker-release ARGS=$(DOCKER_BUILD_ARGS) IMG=$(AV_VENDOR) VERSION=0.0.1 DOCKER_FILE=build/docker/Dockerfile.$(AV_VENDOR) DOCKER_DIR=build/data
 
 multiav-build-av-go: ## Build the AV with the gRPC server
-	sudo make docker-release IMG=go$(AV_VENDOR) VERSION=0.0.1 DOCKER_FILE=core/multiav/$(AV_VENDOR)/Dockerfile DOCKER_DIR=core/multiav/$(AV_VENDOR)/
+	sudo make docker-release IMG=go$(AV_VENDOR) VERSION=0.0.1 DOCKER_FILE=internal/multiav/$(AV_VENDOR)/Dockerfile DOCKER_DIR=internal/multiav/$(AV_VENDOR)/
