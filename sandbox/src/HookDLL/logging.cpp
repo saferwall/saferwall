13bfd6e63bcbe36fa352f752f7445e0f4a44760f
Ayoub Faouzi <lord.noteworthy@protonmail.com>
2019-10-31 08:48:59 +1100

    [sandbox/hookdll] add logging function

---
+++ sandbox/src/HookDLL/logging.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <Windows.h>
+
+
+//
+// Globals
+//
+
+extern __vsnwprintf_fn_t _vsnwprintf;
+extern REGHANDLE ProviderHandle;
+
+
+
+VOID TraceAPI(PCWSTR Format, ...) {
+
+	WCHAR Buffer[256];
+
+	va_list arglist;
+	va_start(arglist, Format);
+	_vsnwprintf(Buffer, RTL_NUMBER_OF(Buffer), Format, arglist);
+	EtwEventWriteString(ProviderHandle, 0, 0, Buffer);
+	va_end(arglist);
+}
+
+
