afec1c2651bc83043e4ba46ea2303ec9bf1ea466
Lord Noteworthy <lord.noteworthy@protonmail.com>
2019-12-07 01:25:01 -0800

    [pkg/yara] add a yara scanner

---
+++ cmd/yara/main.go
@@ -0,0 +1,107 @@
+// Copyright 2019 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/hillu/go-yara"
+	"github.com/saferwall/saferwall/pkg/yara"
+
+	"errors"
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type rules []goyara.Rule
+
+
+func (r *rules) Set(arg string) error {
+	if len(arg) == 0 {
+		return errors.New("empty rule specification")
+	}
+	a := strings.SplitN(arg, ":", 2)
+	switch len(a) {
+	case 1:
+		*r = append(*r, goyara.Rule{Filename: a[0]})
+	case 2:
+		*r = append(*r, goyara.Rule{Namespace: a[0], Filename: a[1]})
+	}
+	return nil
+}
+
+func (r *rules) String() string {
+	var s string
+	for _, rule := range *r {
+		if len(s) > 0 {
+			s += " "
+		}
+		if rule.Namespace != "" {
+			s += rule.Namespace + ":"
+		}
+		s += rule.Filename
+	}
+	return s
+}
+
+func printMatches(m []yara.MatchRule, err error) {
+	if err == nil {
+		if len(m) > 0 {
+			for _, match := range m {
+				log.Printf("- [%s] %s ", match.Namespace, match.Rule)
+			}
+		} else {
+			log.Print("no matches.")
+		}
+	} else {
+		log.Printf("error: %s.", err)
+	}
+}
+
+func main() {
+	var (
+		rules       rules
+		processScan bool
+		pids        []int
+	)
+	flag.BoolVar(&processScan, "processes", false, "scan processes instead of files")
+	flag.Var(&rules, "rule", "add rule")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("no files or processes specified")
+	}
+
+	if processScan {
+		for _, arg := range args {
+			if pid, err := strconv.Atoi(arg); err != nil {
+				log.Fatalf("Could not parse %s ad number", arg)
+			} else {
+				pids = append(pids, pid)
+			}
+		}
+	}
+
+	r, err := goyara.Load(rules)
+	if err != nil {
+		log.Fatalf("Could not parse %s ad number", err)
+	}
+	
+	if processScan {
+		for _, pid := range pids {
+			log.Printf("Scanning process %d...", pid)
+			m, err := r.ScanProc(pid, 0, 0)
+			printMatches(m, err)
+		}
+	} else {
+		for _, filename := range args {
+			log.Printf("Scanning file %s... ", filename)
+			m, err := goyara.ScanFile(r, filename)
+			printMatches(m, err)
+		}
+	}
+}
---
+++ pkg/yara/main.go
@@ -0,0 +1,61 @@
+// Copyright 2019 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package goyara
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hillu/go-yara"
+	"os"
+)
+
+// Rule represents a Yara rule.
+type Rule struct {
+	Namespace string
+	Filename  string
+}
+
+
+// Load and compile yara rules
+func Load(rules []Rule) (*yara.Rules, error) {
+
+	if len(rules) == 0 {
+		return nil, errors.New("no rules specified")
+	}
+
+	c, err := yara.NewCompiler()
+	if err != nil {
+		msg := fmt.Sprintf("failed to initialize yara compiler: %s", err)
+		return nil, errors.New(msg)
+	}
+
+	for _, rule := range rules {
+		f, err := os.Open(rule.Filename)
+		if err != nil {
+			msg := fmt.Sprintf("could not open rule file %s: %s", rule.Filename, err)
+			return nil, errors.New(msg)
+		}
+		err = c.AddFile(f, rule.Namespace)
+		f.Close()
+		if err != nil {
+			msg := fmt.Sprintf("could not parse rule file %s: %s", rule.Filename, err)
+			return nil, errors.New(msg)
+		}
+	}
+
+	r, err := c.GetRules()
+	if err != nil {
+		msg := fmt.Sprintf("failed to compile rules: %s", err)
+		return nil, errors.New(msg)
+	}
+
+	return r, nil
+}
+
+// ScanFile performs a scan over a file path
+func ScanFile(r *yara.Rules, filepath string) ([]yara.MatchRule, error) {
+	m, err := r.ScanFile(filepath, 0, 0)
+	return m, err
+}
