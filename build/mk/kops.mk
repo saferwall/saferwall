9981f7aae0dcb4024478326686b2a697d0b0744f
Lord Noteworthy <lord.noteworthy@protonmail.com>
2019-12-24 13:53:27 -0800

    [deployments] add efs-provisioner

--- build/mk/kops.mk
+++ build/mk/kops.mk
@@ -87,14 +87,3 @@ kops-update-cluster:		## Update k8s cluster
 	kops edit ig --name= nodes
 	kops update cluster --yes
 	kops rolling-update cluster --yes
-
-kops-get-usage:
-	alias util='kubectl get nodes --no-headers | awk '\''{print $1}'\'' | xargs -I {} sh -c '\''echo {} ; kubectl describe node {} | grep Allocated -A 5 | grep -ve Event -ve Allocated -ve percent -ve -- ; echo '\'''
-
-	# Get CPU request total (we x20 because because each m3.large has 2 vcpus (2000m) )
-	alias cpualloc='util | grep % | awk '\''{print $1}'\'' | awk '\''{ sum += $1 } END { if (NR > 0) { print sum/(NR*20), "%\n" } }'\'''
-
-	# Get mem request total (we x75 because because each m3.large has 7.5G ram )
-	alias memalloc='util | grep % | awk '\''{print $5}'\'' | awk '\''{ sum += $1 } END { if (NR > 0) { print sum/(NR*75), "%\n" } }'\'''
-
-	util
