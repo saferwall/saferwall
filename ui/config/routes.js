c59cb8cd7328fdd3c71306f371d1c1f55429acfc
Kerkesni <48165340+Kerkesni@users.noreply.github.com>
2020-01-20 17:14:11 +0100

    Fixed empty reset password page

--- ui/config/routes.js
+++ ui/config/routes.js
@@ -70,7 +70,7 @@ module.exports = {
       },
     },
     RESET_PWD: {
-      path: "/reset_password/",
+      path: "/reset-password/",
       name: "resetPassword",
       meta: {
         title: "Reset Password",
