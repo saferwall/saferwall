a18ec08932b0e2de47a02d5e4b01fe1623d04e1a
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-01-22 18:51:47 +1100

    [deployments] bump nginx-ingress

--- build/mk/kops.mk
+++ build/mk/kops.mk
@@ -104,3 +104,6 @@ kops-init-cert-manager: # Init cert-manager
 	kubectl create namespace cert-manager
 	kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 	kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.12/deploy/manifests/00-crds.yaml
+
+# edit gp2 to: allowVolumeExpansion: true
+
