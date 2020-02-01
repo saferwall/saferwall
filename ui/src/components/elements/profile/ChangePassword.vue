8c63cef2ff0e6a8d0751dacdbb66532a5cd5c4a7
Kerkesni <48165340+Kerkesni@users.noreply.github.com>
2020-02-01 22:11:07 +0100

    Added Password Change

--- ui/src/components/elements/profile/ChangePassword.vue
+++ ui/src/components/elements/profile/ChangePassword.vue
@@ -10,8 +10,18 @@
         v-model.trim="$v.oldPassword.$model"
         type="password"
       />
-      <div class="error" v-if="!$v.oldPassword.required && $v.oldPassword.$dirty">
+      <div
+        class="error"
+        v-if="!$v.oldPassword.required && $v.oldPassword.$dirty"
+      >
         Old Password is required.
+      </div>
+      <div
+        class="error"
+        v-if="!$v.oldPassword.minLength && $v.oldPassword.$dirty"
+      >
+        Password must have at least
+        {{ $v.oldPassword.$params.minLength.min }} letters.
       </div>
     </div>
     <div
@@ -27,6 +37,9 @@
         Password must have at least
         {{ $v.password.$params.minLength.min }} letters.
       </div>
+      <div class="error" v-if="!$v.password.notSame && $v.password.$dirty">
+        Same as old password.
+      </div>
     </div>
     <div
       class="form-group"
@@ -38,16 +51,25 @@
         v-model.trim="$v.repeatPassword.$model"
         type="password"
       />
-      <div class="error" v-if="!$v.repeatPassword.sameAsPassword && $v.repeatPassword.$dirty">
+      <div
+        class="error"
+        v-if="!$v.repeatPassword.sameAsPassword && $v.repeatPassword.$dirty"
+      >
         Passwords must be identical.
       </div>
     </div>
-    <button class="button is-primary is-outlined" :disabled="$v.$invalid">Submit</button>
+    <button
+      class="button is-primary is-outlined"
+      :disabled="$v.$invalid"
+      @click="submit"
+    >
+      Submit
+    </button>
   </div>
 </template>
 
 <script>
-import { required, minLength, sameAs } from "vuelidate/lib/validators"
+import { required, minLength, sameAs, not } from "vuelidate/lib/validators"
 
 export default {
   data() {
@@ -57,13 +79,52 @@ export default {
       repeatPassword: "",
     }
   },
+  methods: {
+    submit: function() {
+      if (this.$v.$invalid) return
+
+      this.$http
+        .put(
+          this.$api_endpoints.USERS +
+            this.$store.getters.getUsername +
+            "/password",
+          {
+            oldpassword: this.oldPassword,
+            newpassword: this.password,
+          },
+        )
+        .then(() => {
+          this.$awn.success("Password Changed Successfully")
+          this.trackSuccess()
+          this.clear()
+        })
+        .catch(() => {
+          this.$awn.alert("A problem occured, try again")
+        })
+    },
+    trackSuccess() {
+      this.$gtag.event("Pwd_change", {
+        event_category: "Information_Change",
+        event_label: "Password Changed",
+        value: 1,
+      })
+    },
+    clear() {
+      this.oldPassword = ""
+      this.password = ""
+      this.repeatPassword = ""
+      this.$v.$reset()
+    },
+  },
   validations: {
     oldPassword: {
       required,
+      minLength: minLength(8),
     },
     password: {
       required,
-      minLength: minLength(6),
+      minLength: minLength(8),
+      notSame: not(sameAs("oldPassword")),
     },
     repeatPassword: {
       sameAsPassword: sameAs("password"),
@@ -74,7 +135,7 @@ export default {
 
 <style lang="scss" scoped>
 .page {
-    margin-left: 5em;
+  margin-left: 5em;
   .form-group {
     display: grid;
     margin-bottom: 1em;
@@ -92,15 +153,15 @@ export default {
     color: rgb(243, 54, 21);
   }
 
-  .button{
-      margin-top: 0.5em;
-      width: 7em;
+  .button {
+    margin-top: 0.5em;
+    width: 7em;
   }
 
-  .form-group--error{
-      .input{
-          border-color: rgb(243, 54, 21);
-      }
+  .form-group--error {
+    .input {
+      border-color: rgb(243, 54, 21);
+    }
   }
 }
 </style>
