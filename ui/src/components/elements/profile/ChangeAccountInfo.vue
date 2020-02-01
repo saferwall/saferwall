9c73c95d1f4a9833b0de9562d45427f7b3ceea9f
Kerkesni <48165340+Kerkesni@users.noreply.github.com>
2020-02-01 23:15:54 +0100

    Added Account Information change

--- ui/src/components/elements/profile/ChangeAccountInfo.vue
+++ ui/src/components/elements/profile/ChangeAccountInfo.vue
@@ -2,17 +2,10 @@
   <div class="page">
     <div class="form-group" :class="{ 'form-group--error': $v.name.$error }">
       <label class="form__label">Name</label>
-      <input class="input" v-model.trim="$v.name.$model" type="text" />
+      <input class="input" v-model.trim="$v.name.$model" type="text" placeholder="Real Name"/>
       <div class="error" v-if="!$v.name.required && $v.name.$dirty">
         Field Required
       </div>
-      <div class="error" v-if="!$v.name.min && $v.name.$dirty">
-        Name should contain at least 5 characters
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="form__label">Email</label>
-      <input class="input" :value="email" type="text" readonly />
     </div>
     <div class="form-group" :class="{ 'form-group--error': $v.bio.$error }">
       <label class="form__label">Bio</label>
@@ -24,9 +17,6 @@
       />
       <div class="error" v-if="!$v.bio.required && $v.bio.$dirty">
         Field Required
-      </div>
-      <div class="error" v-if="!$v.bio.min && $v.bio.$dirty">
-        Bio should contain at least 10 characters
       </div>
       <div class="error" v-if="!$v.bio.max && $v.bio.$dirty">
         Bio should contain at most 200 characters
@@ -41,14 +31,14 @@
       :class="{ 'form-group--error': $v.location.$error }"
     >
       <label class="form__label">Location</label>
-      <input class="input" v-model.trim="$v.location.$model" type="text" />
+      <input class="input" v-model.trim="$v.location.$model" type="text"  placeholder="location"/>
       <div class="error" v-if="!$v.location.required && $v.location.$dirty">
         Field Required
       </div>
     </div>
     <div class="form-group" :class="{ 'form-group--error': $v.URL.$error }">
       <label class="form__label">URL</label>
-      <input class="input" v-model.trim="$v.URL.$model" type="text" />
+      <input class="input" v-model.trim="$v.URL.$model" type="text" placeholder="url"/>
       <div class="error" v-if="!$v.URL.required && $v.URL.$dirty">
         Field Required
       </div>
@@ -56,36 +46,67 @@
         Wrong Format
       </div>
     </div>
-    <button class="button is-primary is-outlined" :disabled="$v.$invalid">
+    <button class="button is-primary is-outlined" :disabled="$v.$invalid" @click="submit">
       Submit
     </button>
   </div>
 </template>
 
 <script>
-import { required, maxLength, minLength, url } from "vuelidate/lib/validators"
+import { required, maxLength, url} from "vuelidate/lib/validators"
 
 export default {
   data() {
     return {
-      name: "walid",
-      email: "walid@gmail.com",
-      bio:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-      member_since: "12/10/2019",
-      location: "France",
-      URL: "/walidO",
+      name: "",
+      bio: "",
+      member_since: "",
+      location: "",
+      URL: "",
     }
+  },
+  methods: {
+    submit: function() {
+      if (this.$v.$invalid) return
+
+      this.$http
+        .put(this.$api_endpoints.USERS + this.$store.getters.getUsername, {
+          name: this.name,
+          bio: this.bio,
+          location: this.location,
+          url: this.URL,
+        })
+        .then(() => {
+          this.$awn.success("Information Changed Successfully")
+          this.trackSuccess()
+          this.clear()
+        })
+        .catch(() => {
+          this.$awn.alert("A problem occured, try again")
+        })
+    },
+    trackSuccess() {
+      this.$gtag.event("Account_information_change", {
+        event_category: "Information_Change",
+        event_label: "Info Changed",
+        value: 1,
+      })
+    },
+    clear() {
+      this.name = ""
+      this.bio = ""
+      this.location = ""
+      this.URL = ""
+      this.$v.$reset()
+    },
   },
   validations: {
     name: {
       required,
-      min: minLength(5),
     },
     bio: {
       required,
-      max: maxLength(200),
-      min: minLength(10),
+      max: maxLength(255),
     },
     location: {
       required,
