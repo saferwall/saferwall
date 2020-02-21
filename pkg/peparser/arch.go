6bba54c49ecf4a85b1c4024a4582337a4049f5ed
LordNoteworthy <lord.noteworthy@protonmail.com>
2020-02-22 10:56:55 +1100

    [pkg/peparser] add forgotten architecture directory file

---
+++ pkg/peparser/arch.go
@@ -0,0 +1,9 @@
+// Copyright 2020 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package pe
+
+func (pe *File) parseArchitectureDirectory(rva, size uint32) error {
+	return nil
+}
