d0246c634b91fe7ab6dfba4c7fb47d9030bfe7e8
LordNoteworthy <lord.noteworthy@protonmail.com>
2020-03-17 18:59:54 +1100

    [sandbox] test network ops

---
+++ sandbox/src/HookTest/test_network.cpp
@@ -0,0 +1,58 @@
+#include "header.h"
+
+
+VOID
+DoNetworkOps()
+{
+    wprintf(L" ========= Testing network opeations ========= \n\n");
+
+	wprintf(L"[+] Calling InternetOpenW\n");
+    HINTERNET hSession = InternetOpen(
+        L"Mozilla/5.0", // User-Agent
+        INTERNET_OPEN_TYPE_PRECONFIG,
+        NULL,
+        NULL,
+        0);
+
+	wprintf(L"[+] Calling InternetConnectW\n");
+    HINTERNET hConnect = InternetConnect(
+        hSession,
+        L"www.google.com", // HOST
+        0,
+        L"",
+        L"",
+        INTERNET_SERVICE_HTTP,
+        0,
+        0);
+
+	wprintf(L"[+] Calling HttpOpenRequestW\n");
+    HINTERNET hHttpFile = HttpOpenRequest(
+        hConnect,
+        L"GET", // METHOD
+        L"/",   // URI
+        NULL,
+        NULL,
+        NULL,
+        0,
+        0);
+
+	wprintf(L"[+] Calling HttpSendRequestW\n");
+    BOOL Success = HttpSendRequest(hHttpFile, NULL, 0, 0, 0);
+    if (Success == FALSE)
+    {
+        wprintf(L"Failed Request\n");
+    }
+
+    HttpSendRequestA(hHttpFile, NULL, 0, 0, 0);
+
+    CHAR szBuffer[4096 * 10] = "";
+    DWORD dwRead = 0;
+    while (InternetReadFile(hHttpFile, szBuffer, sizeof(szBuffer) - 1, &dwRead) && dwRead)
+    {
+        szBuffer[dwRead] = 0;
+        dwRead = 0;
+    }
+    InternetCloseHandle(hHttpFile);
+    InternetCloseHandle(hConnect);
+    InternetCloseHandle(hSession);
+}
