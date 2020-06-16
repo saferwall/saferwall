f3bd59626f38d84600a0ab9efdbfc3531dfe513d
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-06-16 18:52:29 +1000

    [cmd/uploader] improve uploader by adding awareness of obj storage

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -6,8 +6,9 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
+	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -18,12 +19,17 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-	"crypto/sha256"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 const (
 	fileURL = "https://api.saferwall.com/v1/files/"
-	authURL   = "https://api.saferwall.com/v1/auth/login/"
+	authURL = "https://api.saferwall.com/v1/auth/login/"
+	bucket  = "saferwall-samples"
+	region  = "us-east-1"
 )
 
 func check(e error) {
@@ -36,6 +42,25 @@ func getSha256(b []byte) string {
 	h := sha256.New()
 	h.Write(b)
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func difference(a, b []string) []string {
+    mb := make(map[string]struct{}, len(b))
+    for _, x := range b {
+        mb[x] = struct{}{}
+    }
+    var diff []string
+    for _, x := range a {
+        if _, found := mb[x]; !found {
+            diff = append(diff, x)
+        }
+    }
+    return diff
 }
 
 func newfileUploadRequest(uri, fieldname, filename string) (*http.Request, error) {
@@ -132,6 +157,8 @@ func upload(filepath string, authToken string) {
 
 func rescan(sha256 string, authToken string) error {
 
+	log.Printf("rescanning %s\n", sha256)
+
 	payload, err := json.Marshal(map[string]string{
 		"type": "rescan",
 	})
@@ -163,12 +190,35 @@ func rescan(sha256 string, authToken string) error {
 	return nil
 }
 
+func scan(sha256 string, authToken string) error {
 
-func isFileFound(filePath, token string) bool{
-	dat, err := ioutil.ReadFile(filePath)
-	check(err)
-	sha256 := getSha256(dat)
-	
+	log.Printf("Scanning %s\n", sha256)
+
+	url := fileURL + sha256 + "/scan"
+	request, err := http.NewRequest("POST", url, nil)
+	request.Header.Set("Cookie", "JWTCookie="+authToken)
+
+	// Perform the http post request.
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	// Read the response.
+	body := &bytes.Buffer{}
+	_, err = body.ReadFrom(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp.Body.Close()
+	fmt.Println(body)
+	return nil
+}
+
+func isFileFoundInDB(sha256, token string) bool {
+
 	url := fileURL + sha256 + "/?fields=status"
 	resp, err := http.Get(url)
 	if err != nil {
@@ -200,11 +250,111 @@ func isFileFound(filePath, token string) bool{
 			return true
 		}
 	}
-
-	log.Printf("rescanning %s", sha256)
-	rescan(sha256, token)
-	time.Sleep(15 * time.Second)
 	return false
+}
+
+func isFileFoundInObjStorage(sha256 string) bool {
+
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+	_, err := svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(sha256)})
+	if err != nil {
+		log.Printf("svc.HeadObject failed with: %v", err)
+		return false
+	}
+
+	return true
+}
+
+func listObject(bucket, region string, verbose bool) []string {
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
+	check(err)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	// Get the list of items
+	var objKeys []string
+	query := &s3.ListObjectsV2Input{
+		Bucket: &bucket,
+	}
+
+	for {
+		resp, err := svc.ListObjectsV2(query)
+		if err != nil {
+			exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
+		}
+
+		for _, item := range resp.Contents {
+			if verbose {
+				fmt.Println("Name:         ", *item.Key)
+				fmt.Println("Last modified:", *item.LastModified)
+				fmt.Println("Size:         ", *item.Size)
+				fmt.Println("Storage class:", *item.StorageClass)
+				fmt.Println("")
+			}
+
+			objKeys = append(objKeys, *item.Key)
+		}
+
+		// Fetch the next chunk.
+		query.ContinuationToken = resp.NextContinuationToken
+
+		if *resp.IsTruncated == false {
+			break
+		}
+	}
+
+	fmt.Println("Found", len(objKeys), "items in bucket", bucket)
+	fmt.Println("")
+	return objKeys
+}
+
+func listAllFilesInDb(authToken string) ([]string, error) {
+
+	var listSha256 []string
+	url := fileURL + "?fields=sha256"
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Set("Cookie", "JWTCookie="+authToken)
+
+	// Perform the http post request.
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+		return listSha256, err
+	}
+
+	// Read the response.
+	body := &bytes.Buffer{}
+	_, err = body.ReadFrom(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return listSha256, err
+	}
+
+	var shaMap []map[string]string
+	err = json.Unmarshal(body.Bytes(), &shaMap)
+	check(err)
+
+	for _, v := range shaMap {
+		listSha256 = append(listSha256, v["sha256"])
+	}
+
+	resp.Body.Close()
+	return listSha256, nil
 }
 
 func main() {
@@ -215,9 +365,9 @@ func main() {
 	}
 	filePath := os.Args[1]
 	_, err := os.Stat(filePath)
-    if os.IsNotExist(err) {
+	if os.IsNotExist(err) {
 		log.Fatalf("%s does not exist", filePath)
-    }
+	}
 
 	// Get credentials.
 	username := os.Getenv("SAFERWALL_AUTH_USERNAME")
@@ -230,6 +380,13 @@ func main() {
 	token, err := login(username, password)
 	check(err)
 
+	shaListInDB, err := listAllFilesInDb(token)
+	check(err)
+	shaListInObjStorage := listObject(bucket, region, false)
+
+	missingSha := difference(shaListInObjStorage, shaListInDB)
+	fmt.Print(missingSha)
+
 	// Walk over directory.
 	fileList := []string{}
 	filepath.Walk(filePath, func(path string, f os.FileInfo, err error) error {
@@ -240,12 +397,26 @@ func main() {
 	})
 
 	// Upload files
-	for _, file := range fileList {
-		// Check if we have the file already in our database.
-		found := isFileFound(file, token)
-		if !found {
-			upload(file, token)
-			time.Sleep(15 * time.Second)
+	for _, filename := range fileList {
+
+		// Get sha256
+		dat, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Printf("Failed to read %s, reason: %v", filePath, err)
 		}
+		sha256 := getSha256(dat)
+
+		// Check if we have the file already in our database.
+		found := isFileFoundInDB(sha256, token)
+		if !found {
+			if isFileFoundInObjStorage(sha256) {
+				scan(sha256, token)
+			} else {
+				upload(filename, token)
+			}
+		} else {
+			rescan(sha256, token)
+		}
+		time.Sleep(15 * time.Second)
 	}
 }
