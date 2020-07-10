a051f50860e14f9fde3a03cb6a4c5b3b8fc91822
LordNoteworthy <lord.noteworthy@protonmail.com>
2020-07-10 10:16:33 +1000

    [pkg/peparser] parse debug dir (POGO) type

--- pkg/peparser/debug.go
+++ pkg/peparser/debug.go
@@ -71,10 +71,32 @@ const (
 )
 
 const (
-	// CVSignatureRSDS represents the CodeView signature 'SDSR'
+	// CVSignatureRSDS represents the CodeView signature 'SDSR'.
 	CVSignatureRSDS = 0x53445352
-	// CVSignatureNB10 represents the CodeView signature 'NB10'
+
+	// CVSignatureNB10 represents the CodeView signature 'NB10'.
 	CVSignatureNB10 = 0x3031424e
+)
+
+const (
+	// FrameFPO indicates a frame of type FPO.
+	FrameFPO = 0x0
+
+	// FrameTrap indicates a frame of type Trap.
+	FrameTrap = 0x1
+
+	// FrameTSS indicates a frame of type TSS.
+	FrameTSS = 0x2
+
+	// FrameNonFPO indicates a frame of type Non-FPO.
+	FrameNonFPO = 0x3
+)
+
+const (
+	POGOTypePGU  = 0x50475500
+	POGzOTypePGI = 0x50474900
+	POGOTypePGO  = 0x50474F00
+	POGOTypeLTCG = 0x4c544347
 )
 
 // ImageDebugDirectory represents the IMAGE_DEBUG_DIRECTORY structure.
@@ -171,6 +193,50 @@ type CvInfoPDB20 struct {
 	// Null-terminated name of the PDB file. It can also contain full or partial
 	// path to the file.
 	PDBFileName string
+}
+
+// FPOData Represents the stack frame layout for a function on an x86 computer when frame pointer omission (FPO) optimization is used. The structure is used to locate the base of the call frame.
+type FPOData struct {
+	// The offset of the first byte of the function code.
+	OffStart uint32
+
+	// The number of bytes in the function.
+	ProcSize uint32
+
+	// The number of local variables.
+	NumLocals uint32
+
+	// The size of the parameters, in DWORDs.
+	NunParams uint32
+
+	// The number of bytes in the function prolog code.
+	NumProlog uint32
+
+	// The number of registers saved.
+	NumRegs uint8
+
+	// A variable that indicates whether the function uses structured exception handling.
+	HasSEH uint8
+
+	// A variable that indicates whether the EBP register has been allocated.
+	UseBP uint8
+
+	// Reserved for future use.
+	Reserved uint8
+
+	// A variable that indicates the frame type.
+	FrameType uint8
+}
+
+type ImagePGOItem struct {
+	Rva  uint32
+	Size uint32
+	Name string
+}
+
+type POGO struct {
+	Signature uint32 // _IMAGE_POGO_INFO
+	Entries   []ImagePGOItem
 }
 
 // ImageDebugMisc represents the IMAGE_DEBUG_MISC structure.
@@ -288,6 +354,45 @@ func (pe *File) parseDebugDirectory(rva, size uint32) error {
 				// Include these extra informations
 				debugEntry.Info = pdb
 			}
+		case ImageDebugTypePOGO:
+			pogoSignature, err := pe.ReadUint32(debugDir.PointerToRawData)
+			if err != nil {
+				continue
+			}
+
+			pogo := POGO{}
+
+			switch pogoSignature {
+			case POGOTypePGU:
+			case POGzOTypePGI:
+			case POGOTypePGO:
+			case POGOTypeLTCG:
+				pogo.Signature = pogoSignature
+				offset = debugDir.PointerToRawData + 4
+				c := uint32(0)
+				for c < debugDir.SizeOfData-4 {
+
+					pogoEntry := ImagePGOItem{}
+					pogoEntry.Rva, err = pe.ReadUint32(offset)
+					if err != nil {
+						break
+					}
+					pogoEntry.Size, err = pe.ReadUint32(offset + 4)
+					if err != nil {
+						break
+					}
+
+					pogoEntry.Name = string(pe.getStringFromData(0, pe.data[offset+8:offset+8+32]))
+
+					pogo.Entries = append(pogo.Entries, pogoEntry)
+					c += 8 + uint32(len(pogoEntry.Name)) + 4
+					offset += 8 + uint32(len(pogoEntry.Name)) + 4
+				}
+
+				// Include these extra informations
+				debugEntry.Info = pogo
+			}
+
 		case ImageDebugTypeMisc:
 			break
 		}
@@ -298,4 +403,66 @@ func (pe *File) parseDebugDirectory(rva, size uint32) error {
 	}
 
 	return nil
+}
+
+// SectionAttributeDescription maps a section attribute to a friendly name.
+func SectionAttributeDescription(section string) string {
+	sectionNameMap := map[string]string{
+		".CRT$XCA":      "First C++ Initializer",
+		".CRT$XCAA":     "Startup C++ Initializer",
+		".CRT$XCZ":      "Last C++ Initializer",
+		".CRT$XDA":      "First Dynamic TLS Initializer",
+		".CRT$XDZ":      "Last Dynamic TLS Initializer",
+		".CRT$XIA":      "First C Initializer",
+		".CRT$XIAA":     "Startup C Initializer",
+		".CRT$XIAB":     "PGO C Initializer",
+		".CRT$XIAC":     "Post-PGO C Initializer",
+		".CRT$XIC":      "CRT C Initializers",
+		".CRT$XIYA":     "VCCorLib Threading Model Initializer",
+		".CRT$XIYAA":    "XAML Designer Threading Model Override Initializer",
+		".CRT$XIYB":     "VCCorLib Main Initializer",
+		".CRT$XIZ":      "Last C Initializer",
+		".CRT$XLA":      "First Loader TLS Callback",
+		".CRT$XLC":      "CRT TLS Constructor",
+		".CRT$XLD":      "CRT TLS Terminator",
+		".CRT$XLZ":      "Last Loader TLS Callback",
+		".CRT$XPA":      "First Pre-Terminator",
+		".CRT$XPB":      "CRT ConcRT Pre-Terminator",
+		".CRT$XPX":      "CRT Pre-Terminators",
+		".CRT$XPXA":     "CRT stdio Pre-Terminator",
+		".CRT$XPZ":      "Last Pre-Terminator",
+		".CRT$XTA":      "First Terminator",
+		".CRT$XTZ":      "Last Terminator",
+		".CRTMA$XCA":    "First Managed C++ Initializer",
+		".CRTMA$XCZ":    "Last Managed C++ Initializer",
+		".CRTVT$XCA":    "First Managed VTable Initializer",
+		".CRTVT$XCZ":    "Last Managed VTable Initializer",
+		".rtc$IAA":      "First RTC Initializer",
+		".rtc$IZZ":      "Last RTC Initializer",
+		".rtc$TAA":      "First RTC Terminator",
+		".rtc$TZZ":      "Last RTC Terminator",
+		".text$x":       "EH Filters",
+		".text$di":      "MSVC Dynamic Initializers",
+		".text$yd":      "MSVC Destructors",
+		".text$mn":      "Contains EP",
+		".00cfg":        "CFG Check Functions Pointers",
+		".rdata$T":      "TLS Header",
+		".rdata$r":      "RTTI Data",
+		".data$r":       "RTTI Type Descriptors",
+		".rdata$sxdata": "Safe SEH",
+		".rdata$zzzdbg": "Debug Data",
+		".idata$2":      "Import Descriptors",
+		".idata$3":      "Final Null Entry",
+		".idata$4":      "INT Array",
+		".idata$5":      "IAT Array",
+		".idata$6":      "Symbol and DLL names",
+		".rsrc$01":      "Resources Header",
+		".rsrc$02":      "Resources Data",
+	}
+
+	if val, ok := sectionNameMap[section]; ok {
+		return val
+	}
+
+	return "?"
 }
