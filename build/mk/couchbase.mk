fc3db8c30cc30bac1f1adb43ac4292f5d91ec0dc
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-07-19 14:07:13 +1000

    [build] improve couchbase docker makefile

--- build/mk/couchbase.mk
+++ build/mk/couchbase.mk
@@ -1,5 +1,10 @@
-couchbase-run:		## Run couchbase docker container instance.
-	sudo docker run -d --name db -p 8091-8094:8091-8094 -p 11210:11210 couchbase:enterprise-6.5.1
+COUCHBASE_CONTAINER_NAME = cb-db
+COUCHBASE_CONTAINER_STATUS := $(shell sudo docker container inspect -f '{{.State.Status}}' $(COUCHBASE_CONTAINER_NAME))
 
-couchbase-start:	## Start exiting couchbase `db` container.
-	sudo docker start db
+couchbase-start:	## Start Couchbase Server docker container.
+ifeq ($(COUCHBASE_CONTAINER_STATUS),running)
+	@echo "All good, couchabse server is already running."
+else
+	@echo "No stress, creating one ..."
+	sudo docker run -d --name $(COUCHBASE_CONTAINER_NAME) -p 8091-8094:8091-8094 -p 11210:11210 couchbase:enterprise-6.5.1
+endif
