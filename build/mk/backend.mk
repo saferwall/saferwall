012510707e4863879d02b7bef58dd21bbb6a207a
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-07-21 21:11:08 +1000

    [build] retry docker build for backend & consumer

--- build/mk/backend.mk
+++ build/mk/backend.mk
@@ -1,5 +1,16 @@
 backend-build: ## Build backend in a docker container
-	sudo make docker-build IMG=backend DOCKER_FILE=build/docker/Dockerfile.backend DOCKER_DIR=.
+	@echo "${GREEN} [*] =============== Build Backend =============== ${RESET}"
+	sudo make docker-build IMG=backend DOCKER_FILE=build/docker/Dockerfile.backend DOCKER_DIR=. ;
+	@EXIT_CODE=$$?
+	@if test $$EXIT_CODE ! 0; then \
+		sudo make docker-release IMG=backend DOCKER_FILE=build/docker/Dockerfile.backend DOCKER_DIR=. ; \
+	fi
+
 
 backend-release: ## Release backend in a docker container
-	sudo make docker-release IMG=backend VERSION=0.0.2 DOCKER_FILE=build/docker/Dockerfile.backend DOCKER_DIR=.
+	@echo "${GREEN} [*] =============== Build and Release Backend =============== ${RESET}"
+	sudo make docker-release IMG=backend VERSION=0.0.2 DOCKER_FILE=build/docker/Dockerfile.backend DOCKER_DIR=. ;
+	@EXIT_CODE=$$?
+	@if test $$EXIT_CODE ! 0; then \
+		sudo make docker-release IMG=backend VERSION=0.0.2 DOCKER_FILE=build/docker/Dockerfile.backend DOCKER_DIR=. ; \
+	fi
--- build/mk/consumer.mk
+++ build/mk/consumer.mk
@@ -1,5 +1,15 @@
 consumer-build:		## build consumer docker container
-	sudo make docker-build IMG=consumer DOCKER_FILE=build/docker/Dockerfile.consumer DOCKER_DIR=.
+	@echo "${GREEN} [*] =============== Build Consumer =============== ${RESET}"
+	sudo make docker-build IMG=consumer DOCKER_FILE=build/docker/Dockerfile.consumer DOCKER_DIR=. ;
+	@EXIT_CODE=$$?
+	@if test $$EXIT_CODE ! 0; then \
+		sudo make docker-build IMG=consumer VERSION=0.0.2 DOCKER_FILE=build/docker/Dockerfile.consumer DOCKER_DIR=. ; \
+	fi
 
 consumer-release:	## release consumer docker container
-	sudo make docker-release IMG=consumer VERSION=0.0.2 DOCKER_FILE=build/docker/Dockerfile.consumer DOCKER_DIR=.
+	@echo "${GREEN} [*] =============== Build and Release Consumer =============== ${RESET}"
+	sudo make docker-release IMG=consumer DOCKER_FILE=build/docker/Dockerfile.consumer DOCKER_DIR=. ;
+	@EXIT_CODE=$$?
+	@if test $$EXIT_CODE ! 0; then \
+		sudo make docker-release IMG=consumer VERSION=0.0.2 DOCKER_FILE=build/docker/Dockerfile.consumer DOCKER_DIR=. ; \
+	fi
