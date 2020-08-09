d4f2517c62a7f96b96cfbdcb9fea1730756c0d2a
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-08-09 16:08:58 +1000

    [github] add gpg secrets

---
+++ scripts/decrypt_secret.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
+    --output ../.github/secrets/secrets.tar.gz ../.github/secrets/secrets.tar.gz.gpg \
+    && tar zxf ../.github/secrets/secrets.tar.gz -C ../build/data
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
+    --output ../.env ../.github/secrets/.env.gpg
