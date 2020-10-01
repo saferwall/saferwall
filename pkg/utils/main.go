8df1961bfafee36518ffce37899af007f5c48ff7
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-10-01 12:49:04 +1000

    [pkg/utils] add DeleteDirContent() helper func

--- pkg/utils/main.go
+++ pkg/utils/main.go
@@ -296,6 +296,27 @@ func DeleteFile(path string) error {
 	return nil
 }
 
+// DeleteDirContent delete all files from a folder without 
+// deleting the parent directory.
+func DeleteDirContent(dir string) error {
+    d, err := os.Open(dir)
+    if err != nil {
+        return err
+    }
+    defer d.Close()
+    names, err := d.Readdirnames(-1)
+    if err != nil {
+        return err
+    }
+    for _, name := range names {
+        err = os.RemoveAll(filepath.Join(dir, name))
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
 // Download downloads an object from a bucket.
 func Download(client *minio.Client, bucketName, filePath, objectName string) ([]byte, error) {
 	err := client.FGetObject(bucketName, objectName, filePath, minio.GetObjectOptions{})
