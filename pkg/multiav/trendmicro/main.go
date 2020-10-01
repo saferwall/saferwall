2ba4e19c8805a7b6fedab4b961d2fd382b4e9edc
Lord Noteworthy <lord.noteworthy@protonmail.com>
2020-10-01 20:07:29 +1000

    [pkg] add trendmicro in multiav

---
+++ pkg/multiav/trendmicro/main.go
@@ -0,0 +1,154 @@
+// Copyright 2020 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package trendmicro
+
+import (
+	"time"
+	"regexp"
+	"path"
+	"io/ioutil"
+
+	"github.com/saferwall/saferwall/pkg/utils"
+)
+
+const (
+	splxmain = "/opt/TrendMicro/SProtectLinux/SPLX.vsapiapp/splxmain"
+	logDir = "/var/log/TrendMicro/SProtectLinux/"
+	tmsplx = "/opt/TrendMicro/SProtectLinux/tmsplx.xml"
+)
+
+// Result represents detection results
+type Result struct {
+	Infected bool   `json:"infected"`
+	Output   string `json:"output"`
+}
+
+// Version represents all avira components' versions
+type Version struct {
+	EngineVersion string `json:"engine_version"`
+	PatternVersion   string `json:"pattern_version"`
+	SpywarePatternVersion    string `json:"spyware_pattern_version"`
+}
+
+// GetVersion returns Engine, Virus and Patterns versions
+func GetVersion() (Version, error) {
+
+	ver := Version{}
+
+	// The information regarding the versions is found on:
+	// /opt/TrendMicro/SProtectLinux/tmsplx.xml
+	out, err := utils.ReadAll(tmsplx)
+	if err != nil {
+		return Version{}, err
+	}
+	data := string(out)
+
+	// Interesting keys:
+	// <P Name="EngineVersion" Value="12.000.1008"/>
+    // <P Name="PatternVersion" Value="16.259.00"/>
+	// <P Name="SpywarePatternVersion" Value="2.337.00"/>
+	engineReg := regexp.MustCompile(`<P Name="EngineVersion" Value="(.*)"/>`)
+	l := engineReg.FindStringSubmatch(data)
+	if len(l) > 0 {
+		ver.EngineVersion = l[1]
+	}
+
+	patternReg := regexp.MustCompile(`<P Name="PatternVersion" Value="(.*)"/>`)
+	l = patternReg.FindStringSubmatch(data)
+	if len(l) > 0 {
+		ver.PatternVersion = l[1]
+	}
+
+	spywareReg := regexp.MustCompile(`<P Name="SpywarePatternVersion" Value="(.*)"/>`)
+	l = spywareReg.FindStringSubmatch(data)
+	if len(l) > 0 {
+		ver.SpywarePatternVersion = l[1]
+	}
+
+	return ver, nil
+}
+
+// ScanFile scans a given file.
+func ScanFile(filepath string) (Result, error) {
+
+	res := Result{}
+
+	// TrendMicro does not really provide a simple way to grab the results
+	// We need to read it from a log file. We start by deleting all files
+	// from previous scan results.
+	err := utils.DeleteDirContent(logDir)
+	if err != nil {
+		return res, err
+	}
+
+	// splxmain does not seem to be able to scan a file directly,
+	// it only take a directory as argument.
+	// So we create a tmp dir and we make a copy of the file.
+	tempDir, err := ioutil.TempDir("/tmp", "trendmicro")
+	if err != nil {
+		return res, err
+	}
+
+	filename := path.Base(filepath)
+	filePathCopy := path.Join(tempDir, filename)
+	err = utils.CopyFile(filepath, filePathCopy)
+	if err != nil {
+		return res, err
+	}
+
+	// Execute the scanner with the given file path.
+	out, err := utils.ExecCommand(splxmain, "-m", path.Dir(filePathCopy))
+	if err != nil  {
+		return res, err
+	}
+
+	// The logs are found in files that have the following pattern:
+	// Virus.20201001.0001 or Spyware.20201001.0001.
+	currentTime := time.Now()
+	todayDate := currentTime.Format("20060102")
+	virusLog := path.Join(logDir, "Virus." + todayDate + ".0001")
+
+	// The content of the Virus.XYZ and Spyware.XYZ log looks like:
+	// ...
+	// first_action=0
+	// virus_name=Ransom.Win32.GANDCRAB.SMLV2.hp
+	// function_code=12
+	// ...
+	re := regexp.MustCompile("virus_name=(.*)")
+	
+	// Read the Virus.XYZ log
+	data, virusErr := utils.ReadAll(virusLog)
+	if virusErr == nil && len(data) > 0 {
+		out = string(data)
+		l := re.FindStringSubmatch(out)
+		if len(l) > 0 {
+			res.Output = l[1]
+			res.Infected = true
+			return res, nil
+		}
+	}
+
+	// Might be a spyware.
+	spywareLog := path.Join(logDir, "Spyware." + todayDate + ".0001")
+	data, spywareErr := utils.ReadAll(spywareLog)
+	if spywareErr == nil && len(data) > 0{
+		out = string(data)
+		l := re.FindStringSubmatch(out)
+		if len(l) > 0 {
+			res.Output = l[1]
+			res.Infected = true
+			return res, nil
+		}
+	}
+
+	if virusErr != nil {
+		return Result{}, virusErr
+	}
+	if spywareErr != nil {
+		return Result{}, spywareErr
+	}
+
+	return res, nil
+}
---
+++ pkg/multiav/trendmicro/main_test.go
@@ -0,0 +1,39 @@
+// Copyright 2020 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package trendmicro
+
+import (
+	"path"
+	"testing"
+
+	"github.com/saferwall/saferwall/pkg/utils"
+)
+
+type filePathTest struct {
+	filepath string
+	want     Result
+}
+
+func getAbsoluteFilePath(testfile string) string {
+	return path.Join(utils.GetRootProjectDir(), testfile)
+}
+
+var filepathScanTest = []filePathTest{
+	{getAbsoluteFilePath("test/multiav/clean/eicar.com"), Result{Infected: true, Output: "Eicar_test_file"}},
+}
+
+func TestScanFilePath(t *testing.T) {
+	for _, tt := range filepathScanTest {
+		t.Run(tt.filepath, func(t *testing.T) {
+			got, err := ScanFile(tt.filepath)
+			if err != nil {
+				t.Fatalf("TestScanFilePath(%s) failed, err: %s", tt.filepath, err)
+			}
+			if got != tt.want {
+				t.Errorf("TestScanFilePath(%s) got %v, want %v", tt.filepath, got, tt.want)
+			}
+		})
+	}
+}
