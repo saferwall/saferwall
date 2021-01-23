e9920d5e0da909dcf196f55f8e4b6288a5e07f1c
Lord Noteworthy <lord.noteworthy@protonmail.com>
2021-01-23 14:35:17 +1100

    [build] add install couchbase make cmd

--- build/mk/couchbase.mk
+++ build/mk/couchbase.mk
@@ -8,3 +8,10 @@ else
 	@echo "No stress, creating one ..."
 	sudo docker run -d --name $(COUCHBASE_CONTAINER_NAME) -p 8091-8094:8091-8094 -p 11210:11210 couchbase:enterprise-6.5.1
 endif
+
+couchabse-install:	## Install Couchbase in Ubuntu
+	curl -O https://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-amd64.deb
+	sudo dpkg -i ./couchbase-release-1.0-amd64.deb
+	sudo apt-get update
+	sudo apt-get install couchbase-server -y
+
