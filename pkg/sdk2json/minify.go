6a53cb1a1f8dd6a792dcc7516badeb084945b03f
LordNoteworthy <lord.noteworthy@protonmail.com>
2021-03-12 10:00:53 +1100

    [pkg/sdk2json] struct/union minified

--- pkg/sdk2json/minify.go
+++ pkg/sdk2json/minify.go
@@ -5,7 +5,6 @@
 package main
 
 import (
-	"log"
 	"regexp"
 	"strings"
 )
@@ -20,10 +19,10 @@ const (
 // StructUnionMemberMini represents a struct or a union member.
 type StructUnionMemberMini struct {
 	Name       string          `json:"name"`
-	X86Offset  int             `json:"x86off"`
-	X86Size    int             `json:"x86size"`
-	X64Offset  int             `json:"x64off"`
-	X64Size    int             `json:"x64size"`
+	X86Offset  uint8           `json:"x86off"`
+	X86Size    uint8           `json:"x86size"`
+	X64Offset  uint8           `json:"x64off"`
+	X64Size    uint8           `json:"x64size"`
 	Type       uint8           `json:"type"` // Help interpret the value.
 	Definition StructUnionMini `json:"def,omitempty"`
 }
@@ -32,8 +31,8 @@ type StructUnionMemberMini struct {
 type StructUnionMini struct {
 	Name    string                  `json:"name"`
 	Members []StructUnionMemberMini `json:"members"`
-	X86Size int                     `json:"x86size"`
-	X64Size int                     `json:"x64size"`
+	X86Size uint8                   `json:"x86size"`
+	X64Size uint8                   `json:"x64size"`
 }
 
 // APIParamMini represents a paramter of a Win32 API.
@@ -41,7 +40,7 @@ type APIParamMini struct {
 	Annotation        uint8  `json:"anno"`
 	Type              uint8  `json:"type"`
 	Name              string `json:"name"`
-	BufferSizeOrIndex int8   `json:"buffsize_or_idx"`
+	BufferSizeOrIndex uint8   `json:"buffsize_or_idx"`
 }
 
 // APIMini represents information about a Win32 API.
@@ -86,18 +85,18 @@ func getNameFromAnnotation(param APIParam) string {
 }
 
 func getBytePtrIndex(api API, param APIParam, dt dataType,
-	parammini *APIParamMini) int8 {
+	parammini *APIParamMini) uint8 {
 	if dt.Kind == typeBytePtr {
-		log.Printf("API: %s, Name: %s, Type: %s, Anno: %s\n", api.Name,
-			param.Name, param.Type, param.Annotation)
+		// log.Printf("API: %s, Name: %s, Type: %s, Anno: %s\n", api.Name,
+		// 	param.Name, param.Type, param.Annotation)
 
 		name := getNameFromAnnotation(param)
 		if strings.HasPrefix(name, "*") {
 			name = name[1:]
 		}
-		log.Println(name)
+		// log.Println(name)
 		idx := findParamIndexByName(api, name)
-		return int8(idx)
+		return uint8(idx)
 
 	} else if dt.Name == "LPVOID" {
 		// Unfortunately MS is not really consistent about data types.
@@ -105,14 +104,14 @@ func getBytePtrIndex(api API, param APIParam, dt dataType,
 		// []bytes, we try to make a guess based on the annotation.
 		name := getNameFromAnnotation(param)
 		if len(name) > 0 {
-			log.Printf("API: %s, Name: %s, Type: %s, Anno: %s\n", api.Name,
-				param.Name, param.Type, param.Annotation)
+			// log.Printf("API: %s, Name: %s, Type: %s, Anno: %s\n", api.Name,
+			// 	param.Name, param.Type, param.Annotation)
 			if strings.HasPrefix(name, "*") {
 				name = name[1:]
 			}
 			idx := findParamIndexByName(api, name)
 			parammini.Type = typeBytePtr
-			return int8(idx)
+			return uint8(idx)
 		}
 
 	}
@@ -172,4 +171,55 @@ func minifyAPIs(apis map[string]map[string]API) map[string]map[string]APIMini {
 	}
 
 	return mapis
+}
+
+func minifyStructAndUnions(winStructs []Struct) []StructUnionMemberMini {
+	structsAndUnionsMini := []StructUnionMemberMini{}
+	for _, winStruct := range winStructs {
+		structUnionMini := StructUnionMini{}
+		structUnionMini.Name = winStruct.Name
+
+		//
+		// Because of structure padding, the code below calculates the index of
+		// where each individual member is located as well as the size of the
+		// structure in both x86 and x64.
+		// Here are a few rules the Microsoft C compiler arranges structures in
+		// memory:
+		//
+		// 1. each individual member, there will be padding so that to make it
+		// start at an address that is divisible by its size.
+		// e.g on 64 bit system,int should start at address divisible by 4, and
+		// long by 8, short by 2.
+		// 2. char and char[] are special, could be any memory address, so they
+		// don't need padding before them.
+		// 3. For struct, other than the alignment need for each individual
+		// member, the size of whole struct itself will be aligned to a size
+		// divisible by size of largest individual member, by padding at end.
+		// e.g if struct's largest member is long then divisible by 8, int then
+		// by 4, short then by 2.
+		//
+
+		x86Size := uint8(0)
+		x64Size := uint8(0)
+		x86Offset := uint8(0)
+		x64Offset := uint8(0)
+		largestMemSizex86, largestMemSizex64 := winStruct.Max()
+		for i, winStructMember := range winStruct.Members {
+			miniMember := StructUnionMemberMini{}
+			miniMember.Name = winStructMember.Name
+			miniMember.X86Offset = x86Offset
+			miniMember.X64Offset = x64Offset
+			miniMember.X86Size, miniMember.X64Size = winStructMember.Size()
+			countBytesPaddingx86, countBytesPaddingx64 := winStruct.structMemberPadding(i, largestMemSizex86, largestMemSizex64)
+			x86Offset += countBytesPaddingx86 + miniMember.X86Size
+			x64Offset += countBytesPaddingx64 + miniMember.X64Size
+			x86Size += miniMember.X86Size + countBytesPaddingx86
+			x64Size += miniMember.X64Size + countBytesPaddingx64
+
+		}
+		structUnionMini.X86Size = x86Size
+		structUnionMini.X64Size = x64Size
+	}
+
+	return structsAndUnionsMini
 }
