1c7e578a0b2fcd5a3a0cf0a7515637c27d3fb21f
LordNoteworthy <lord.noteworthy@protonmail.com>
2021-03-13 21:21:06 +1100

    move the ui to a separate repo: saferwall/saferwall-ui

--- ui/config/endpoints.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  FILES: "/v1/files/",
-  USERS: "/v1/users/",
-  AUTH_LOGIN: "/v1/auth/login/",
-  AUTH_REGISTER: "/v1/users/",
-  AUTH_CHANGE_PWD: "/v1/users/password/",
-  CONFIRM_EMAIL: "/v1/auth/resend-confirmation/",
-}
