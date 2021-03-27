4f064a5201dbbe76a240759d33a3058c2cf194cf
Lord Noteworthy <lord.noteworthy@protonmail.com>
2021-03-28 10:37:57 +1100

    [github/workflow] install custom action runner
    [github/workflow] install custom action runner 2
    
    [github/workflow] install custom action runner 3
    
    [github/workflow] install custom action runner 4
    
    [pkg/consumer] incorporate ml prediction results

---
+++ build/mk/github.mk
@@ -0,0 +1,15 @@
+ACTION_RUNNER_ARCH = x64
+ACTION_RUNNER_VER = 2.277.1
+ACTION_RUNNER_ARCHIVE = actions-runner-linux-$(ACTION_RUNNER_ARCH)-$(ACTION_RUNNER_VER).tar.gz
+
+gh-action-runner:	## Install Github action custom runner.
+	mkdir -p actions-runner && cd actions-runner
+	curl -O -L https://github.com/actions/runner/releases/download\
+	/v$(ACTION_RUNNER_VER)/actions-runner-linux-$(ACTION_RUNNER_ARCH)\
+	-$(ACTION_RUNNER_VER).tar.gz
+	tar xzf ./$(ACTION_RUNNER_ARCHIVE)
+	rm $(ACTION_RUNNER_ARCHIVE)
+	./config.sh --url $(GITHUB_REPO) --token $(ACTION_RUNNER_TOKEN)
+
+gh-action-run:		## Run the Github action custom runner.
+	./run.sh
