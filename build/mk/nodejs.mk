beea78fd26938b51535a992aa9688f51c04e4ce0
LordNoteworthy <lord.noteworthy@protonmail.com>
2021-08-03 14:02:35 +1000

    feat: docker release workflow on matrix services

--- build/mk/nodejs.mk
+++ build/mk/nodejs.mk
@@ -1,5 +1,5 @@
 nodejs-install:		## Install nodejs
-	curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+	curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 	sudo apt-get install -y nodejs
 	node -v
 	npm -v
