09724e94a794b95d5bd8499a6241c6d5e09cdfed
LordNoteworthy <lord.noteworthy@protonmail.com>
2021-09-30 07:28:43 +1000

    feat: basic test for avlavel

--- pkg/avlabel/avlabel.go
+++ pkg/avlabel/avlabel.go
@@ -8,6 +8,13 @@ import (
 	"regexp"
 )
 
+type Detection struct {
+	Family   string
+	Category string
+	Platform string
+	Variant  string
+}
+
 func getParams(regEx, url string) (paramsMap map[string]string) {
 
 	var compRegEx = regexp.MustCompile(regEx)
@@ -23,43 +30,71 @@ func getParams(regEx, url string) (paramsMap map[string]string) {
 }
 
 // ParseWindefender parse.
-func ParseWindefender(detection string) map[string]string {
+func ParseWindefender(detection string) Detection {
 	// Backdoor:Win32/Beastdoor.DQ
 	// Exploit:O97M/CVE-2017-11882.M
-	params := getParams(`^(?P<Category>[a-zA-Z]{1,20})\:(?P<Platform>[a-zA-Z0-9]{1,20})\/(?P<Family>[a-zA-Z0-9-]{1,20})\.(?P<Variant>[a-zA-Z0-9]{1,10})$`, detection)
-	return params
+	params := getParams(`^(?P<Category>[a-zA-Z]{1,20})\:(?P<Platform>[a-zA-Z0-9]{1,20})\/(?P<Family>[\w-]{1,20})(?P<Variant>.*)$`, detection)
+	return Detection{
+		Category: params["Category"],
+		Platform: params["Platform"],
+		Family:   params["Family"],
+		Variant:  params["Variant"],
+	}
 }
 
 // ParseEset parse.
-func ParseEset(detection string) map[string]string {
+func ParseEset(detection string) Detection {
 	// Win32/Yurist, Win32/Agobot, IRC/SdBot
 	params := getParams(`^(?P<Platform>[a-zA-Z0-9]{3,10})\/(?P<Family>[a-zA-Z0-9]{1,20})$`, detection)
+
 	if len(params) != 0 {
-		return params
+		return Detection{
+			Platform: params["Platform"],
+			Family:   params["Family"],
+			Variant:  params["Variant"],
+		}
 	}
 
 	// Win32/Agent.ODC, Win32/Injector.DXDY, Win32/Dridex.BC
 	params = getParams(`^(?P<Platform>[a-zA-Z0-9]{3,10})\/(?P<Family>[a-zA-Z0-9]{1,20})\.(?P<Variant>[a-zA-Z0-9]{1,10})$`, detection)
 	if len(params) != 0 {
-		return params
+		return Detection{
+			Platform: params["Platform"],
+			Family:   params["Family"],
+			Variant:  params["Variant"],
+		}
 	}
 
 	// Android/TrojanDropper.Agent.BII, Win32/PSW.OnLineGames.NMY, Win32/Adware.FakeAV.R, Win32/Filecoder.CryptoWall.D
 	params = getParams(`^(?P<Platform>[a-zA-Z0-9]{3,10})\/(?P<Category>[a-zA-Z0-9]{1,20})\.(?P<Family>[a-zA-Z0-9]{1,10})\.(?P<Variant>[a-zA-Z0-9]{1,10})$`, detection)
-	return params
+	return Detection{
+		Platform: params["Platform"],
+		Category: params["Category"],
+		Family:   params["Family"],
+		Variant:  params["Variant"],
+	}
 }
 
 // ParseAvira parses.
-func ParseAvira(detection string) map[string]string {
+func ParseAvira(detection string) Detection {
 	// TR/PSW.Tepfer.ockxa, TR/Patched.Ren.Gen, TR/Crypt.XPACK.Gen
 	// TR/AD.Kreen.blxny
 	params := getParams(`^(?P<Category>[a-zA-Z0-9]{2,10})\/(?P<Type>[a-zA-Z0-9]{1,20})\.(?P<Family>[a-zA-Z0-9]{1,10})\.(?P<Variant>[a-zA-Z0-9]{1,10})$`, detection)
 	if len(params) != 0 {
-		return params
+		return Detection{
+			Category: params["Category"],
+			Platform: params["Type"],
+			Family:   params["Family"],
+			Variant:  params["Variant"],
+		}
 	}
 
 	// HEUR/AGEN.1012588, Linux/Mirai.bonb, KIT/Exploit-M-022.B
 	// EXP/CVE-2017-11882.Gen
 	params = getParams(`^(?P<Category>[a-zA-Z0-9]{2,10})\/(?P<Family>[a-zA-Z0-9-]{1,20})\.(?P<Variant>[a-zA-Z0-9]{1,10})$`, detection)
-	return params
+	return Detection{
+		Category: params["Category"],
+		Family:   params["Family"],
+		Variant:  params["Variant"],
+	}
 }
---
+++ pkg/avlabel/avlabel_test.go
@@ -0,0 +1,91 @@
+// Copyright 2021 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package avlabel
+
+import "testing"
+
+func TestParseWindefender(t *testing.T) {
+	t.Run("TestParseWindefender", func(t *testing.T) {
+		testCases := []struct {
+			in  string
+			out Detection
+		}{
+			{
+				in:  "ALF:HeraklezEval:TrojanSpy:AndroidOS/Anubis.B!rfn",
+				out: Detection{},
+			},
+			{
+				in:  "!#HSTR:AntiDisasmTrick",
+				out: Detection{},
+			},
+			{
+				in:  "!#FOP:Ogimant!Obfuscator!Acv!Ep",
+				out: Detection{},
+			},
+			{
+				in:  "!#Lowfi:AGGREGATOR:BanloadXorPrev6",
+				out: Detection{},
+			},
+			{
+				in: "Virus:DOS/4368",
+				out: Detection{Category: "Virus", Platform: "DOS",
+					Family: "4368"},
+			},
+			{
+				in: "Virus:DOS/Screaming_Fist.894.dr",
+				out: Detection{Category: "Virus", Platform: "DOS",
+					Family: "Screaming_Fist", Variant: ".894.dr"},
+			},
+			{
+				in: "Virus:Win32/Bolzano_4096.A",
+				out: Detection{Category: "Virus", Platform: "Win32",
+					Family: "Bolzano_4096", Variant: ".A"},
+			},
+			{
+				in: "Worm:Win32/Buchon.F@mm",
+				out: Detection{Category: "Worm", Platform: "Win32",
+					Family: "Buchon", Variant: ".F@mm"},
+			},
+			{
+				in: "Worm:Win32/Korgo.AZ.dam#2",
+				out: Detection{Category: "Worm", Platform: "Win32",
+					Family: "Korgo", Variant: ".dam#2"},
+			},
+			{
+				in: "Constructor:Win32/Smoothie_2_001",
+				out: Detection{Category: "Constructor", Platform: "Win32",
+					Family: "Smoothie_2_001",
+				},
+			},
+			{
+				in: "Trojan:MacOS/Exploit.CVE-2007-6166!MTB",
+				out: Detection{Category: "Trojan", Platform: "MacOS",
+					Family: "Exploit", Variant: ".CVE-2007-6166!MTB"},
+			},
+			{
+				in: "TrojanDownloader:MSIL/Gendwnurl.BL!bit",
+				out: Detection{Category: "TrojanDownloader", Platform: "MSIL",
+					Family: "Gendwnurl", Variant: ".BL!bit"},
+			},
+			{
+				in: "Exploit:O97M/CVE-2017-11882.PDD!MTB",
+				out: Detection{Category: "Exploit", Platform: "O97M",
+					Family: "CVE-2017-11882", Variant: ".PDD!MTB"},
+			},
+			{
+				in: "Spammer:Win32/MailSender.2_2",
+				out: Detection{Category: "Spammer", Platform: "Win32",
+					Family: "MailSender", Variant: ".2_2"},
+			},
+		}
+
+		for _, tt := range testCases {
+			got := ParseWindefender(tt.in)
+			if got.Family != tt.out.Family {
+				t.Errorf("TestParseWindefender(%s) got %v, want %v", tt.in, got, tt.out)
+			}
+		}
+	})
+}
