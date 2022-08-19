458000b3be470aa354988c522f6b0c4827624ad2
Noteworthy <lord.noteworthy@protonmail.com>
2022-08-19 21:58:54 +1000

    Curate PE scan results (#378)
    * feat: curate PE scan results
    
    * update compose and minio configs for dev
    
    * remove inlets-operator
    
    * update .env

--- build/mk/nsq.mk
+++ build/mk/nsq.mk
@@ -1,4 +1,4 @@
-NSQ_ZIP = nsq-1.2.0.linux-amd64.go1.12.9
+NSQ_ZIP = nsq-1.2.1.linux-amd64.go1.16.6
 NSQ_URL = https://s3.amazonaws.com/bitly-downloads/nsq/$(NSQ_ZIP).tar.gz
 
 nsq-install:	## Install NSQ
