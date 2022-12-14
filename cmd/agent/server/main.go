739bdf5bdc76081580034b8fbebba544a1596fbd
Noteworthy <lord.noteworthy@protonmail.com>
2022-12-14 13:09:50 +1100

    fix (sandbox): sent sandbox payload results + fix build go pkg (#391)
    * chore: add more entries to dockerignore
    
    * jsonl2json should return a slice of bytes
    
    * chore(ci): set the context in the build docker action
    
    * remove the multiple workers config from kind
    
    * dont test cmd agent server
    
    * exclude cmd agent server to build on os besides windows

--- cmd/agent/server/main.go
+++ cmd/agent/server/main.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by Apache v2 license
 // license that can be found in the LICENSE file.
 
+// +build windows
+
 // Package server implements a server for AgentServer service.
 package main
 
