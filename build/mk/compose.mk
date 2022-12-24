7ae644f9e3697b91fe8df00f598e743222337773
LordNoteworthy <lord.noteworthy@protonmail.com>
2022-12-24 20:53:05 +1100

    fix shield badge in README and docker compose list services command

--- build/mk/compose.mk
+++ build/mk/compose.mk
@@ -3,4 +3,4 @@ dc-pull: ## Make docker-compose retrieves the lastest images of all services.
 
 dc-up: ##  Start docker-compose (args: SVC, name of the service to exclude)
 	@echo "${GREEN} [*] =============== Docker Compose UP =============== ${RESET}"
-	docker compose ps --services | grep -v '${SVC}\|ml\|comodo\|clamav' | xargs docker-compose up
+	docker compose config --services | grep -v '${SVC}\|ml\|comodo\|clamav' | xargs docker compose up
