a64c87948078c0ace065144f29754fb856349b04
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-04-23 13:55:41 +1000

    bump vagrant

--- build/mk/vbox.mk
+++ build/mk/vbox.mk
@@ -1,13 +1,12 @@
 dist = $$(lsb_release -cs)
 
 vbox-install:		## Install VirtualBox
-	@vboxmanage --version | grep 6.1; \
+	@vboxmanage --version ; \
 		if [ $$? -eq 1 ]; then \
-			wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add - ; \
-			wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - ; \
-			sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(dist) contrib" ; \
+			wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg ; \
+			echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian $(dist) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list ; \
 			sudo apt-get update ; \
-			sudo apt-get install virtualbox-6.1 -y ; \
+			sudo apt-get install virtualbox-7.0 -y ; \
 			sudo apt-get install -f ; \
 		else \
 			echo "${GREEN} [*] VirtualBox already installed ${RESET}"; \
