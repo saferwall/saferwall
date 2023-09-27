ea0ca4e6405a85e8fb8445d6cce14fbda71616da
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-09-27 18:12:18 +1000

    update utils

--- services/sandbox/lua/utils.lua
+++ services/sandbox/lua/utils.lua
@@ -58,4 +58,84 @@ function M.table_copy(obj, seen)
     return setmetatable(res, getmetatable(obj))
 end
 
+--- Filter a table using a predicate function.
+function M.table_filter(func, t)
+    local rettab = {}
+    for _, entry in pairs(t) do
+        if func(entry) then
+            table.insert(rettab, entry)
+        end
+    end
+    return rettab
+end
+
+--- Create a flat list of all files in a directory
+-- @param directory - The directory to scan (default value = './')
+-- @param recursive - Whether or not to scan subdirectories recursively (default value = true)
+-- @param extensions - List of extensions to collect, if blank all will be collected
+function M.scan_dir(directory, recursive, extensions)
+    directory = directory or ''
+    recursive = recursive or true
+
+    local currentDirectory = directory
+    local fileList = {}
+    local command = 'ls ' .. currentDirectory .. ' -p'
+
+    -- if string.sub(directory, -1) ~= '/' then directory = directory .. '/' end
+    if recursive then
+        command = command .. 'R'
+    end
+
+    for fileName in io.popen(command):lines() do
+        if string.sub(fileName, -1) == '/' then
+            -- Directory, don't do anything
+        elseif string.sub(fileName, -1) == ':' then
+            currentDirectory = string.sub(fileName, 1, -2)
+            -- if currentDirectory ~= directory then
+            currentDirectory = currentDirectory .. '/'
+            -- end
+        elseif string.len(fileName) == 0 then
+            -- Blank line
+            currentDirectory = directory
+            -- elseif string.find(fileName,"%.lua$") then
+            -- File is a .lua file
+        else
+            if type(extensions) == 'table' then
+                for _, extension in ipairs(extensions) do
+                    if string.find(fileName, '%.' .. extension .. '$') then
+                        table.insert(fileList, currentDirectory .. fileName)
+                    end
+                end
+            else
+                table.insert(fileList, currentDirectory .. fileName)
+            end
+        end
+    end
+
+    return fileList
+end
+
+function M.path_base_name(file)
+    return file:match('^.+/(.+)$')
+end
+
+function M.split(pString, pPattern)
+    local Table = {} -- NOTE: use {n = 0} in Lua-5.0
+    local fpat = '(.-)' .. pPattern
+    local last_end = 1
+    local s, e, cap = pString:find(fpat, 1)
+    while s do
+        if s ~= 1 or cap ~= '' then
+            table.insert(Table, cap)
+        end
+        last_end = e + 1
+        s, e, cap = pString:find(fpat, last_end)
+    end
+    if last_end <= #pString then
+        cap = pString:sub(last_end)
+        table.insert(Table, cap)
+    end
+    return Table
+end
+
 return M
