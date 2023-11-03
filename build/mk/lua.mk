6ba1b539b73fdf59c39a639d383b4efcb6249ff3
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-11-03 17:34:31 +1100

    got golang binding for lua working

--- build/mk/lua.mk
+++ build/mk/lua.mk
@@ -8,27 +8,3 @@ install-lua: ## Install lua
 		&& make linux test \
 		&& sudo make install
 	rm lua-${LUA_VER}.tar.gz && rm -rf lua-${LUA_VER}
-
-LUAROCKS_VER = 3.9.1
-install-luarocks: ## Install luarocks
-	wget https://luarocks.org/releases/luarocks-${LUAROCKS_VER}.tar.gz
-	tar zxpf luarocks-${LUAROCKS_VER}.tar.gz
-	cd luarocks-${LUAROCKS_VER} \
-		&& ./configure --lua-version=5.4 \
-		&& make \
-		&& sudo make install
-	rm luarocks-${LUAROCKS_VER}.tar.gz
-	rm -rf luarocks-${LUAROCKS_VER}
-
-LUAROCKS_DIR=${ROOT_DIR}/services/sandbox/lua/.luarocks
-install-lua-deps: ## Install lua dependencies
-	luarocks install --tree ${LUAROCKS_DIR} luacov
-	luarocks install --tree ${LUAROCKS_DIR} serpent
-	luarocks install --tree ${LUAROCKS_DIR} luaunit
-	luarocks install --tree ${LUAROCKS_DIR} argparse
-	luarocks install --tree ${LUAROCKS_DIR} rapidjson
-	luarocks install --tree ${LUAROCKS_DIR} luacheck
-	luarocks install --tree ${LUAROCKS_DIR} luafilesystem
-	luarocks install --tree ${LUAROCKS_DIR} dumbluaparser
-	luarocks install --tree ${LUAROCKS_DIR} luacov-console
-	luarocks install --tree ${LUAROCKS_DIR} luaformatter
--- cmd/behavior/behavior.go
+++ cmd/behavior/behavior.go
@@ -6,9 +6,7 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -16,8 +14,7 @@ import (
 	"github.com/saferwall/saferwall/internal/constants"
 	"github.com/saferwall/saferwall/internal/log"
 	"github.com/saferwall/saferwall/internal/utils"
-	"github.com/saferwall/saferwall/services/sandbox"
-	lua "github.com/yuin/gopher-lua"
+	"github.com/stevedonovan/luar"
 )
 
 const (
@@ -47,35 +44,40 @@ func run(logger log.Logger) error {
 	sha256 := parts[len(parts)-3]
 	logger.Infof("processing behavior report for %s : %s", sha256, guid)
 
-	L := lua.NewState()
-	defer L.Close()
-
-	if err := L.DoFile("load.lua"); err != nil {
-		panic(err)
-	}
-
 	// Parse the API Trace JSON file.
 	JSONAPITrace, err := utils.ReadAll(filepath.Join(*flagBhvReportPath, APITraceFilename))
 	if err != nil {
 		return err
 	}
 
-	if err := L.CallByParam(lua.P{
-		Fn:      L.GetGlobal("Eval"),
-		NRet:    1,
-		Protect: true,
-	}, lua.LString(JSONAPITrace)); err != nil {
-		panic(err)
-	}
-	ret := L.Get(-1) // returned value
-	L.Pop(1)         // remove received value
-	fmt.Print(ret)
+	// Init the lua state.
+	L := luar.Init()
+	defer L.Close()
 
-	var w32APIs []sandbox.Win32API
-	err = json.Unmarshal(JSONAPITrace, &w32APIs)
+	// Append the lua dependencies CPATH.
+	L.DoString("package.cpath = package.cpath .. ';./lib/?.so'")
+
+	// Execute our behavior lua code.
+	err = L.DoFile("behavior.lua")
 	if err != nil {
+		logger.Errorf("DoFile failed with: %v", err)
 		return err
 	}
+
+	// Run the rule matching.
+	eval := luar.NewLuaObjectFromName(L, "Eval")
+	defer eval.Close()
+
+	// Using `Call` we would get a generic `[]interface{}`, which is awkward to
+	// work with. But the return type can be specified:
+	results := make([]interface{}, 1)
+	err = eval.Call(&results, string(JSONAPITrace))
+	if err != nil {
+		logger.Errorf("eval.Call failed with: %v", err)
+		return err
+	}
+
+	logger.Info(results[0])
 
 	return nil
 }
