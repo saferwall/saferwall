3a1d76386e4acfc059dfbe8f8fbca5ae65134905
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-11-04 21:15:17 +1100

    install lua

--- build/mk/lua.mk
+++ build/mk/lua.mk
@@ -1,6 +1,6 @@
 
 LUA_VER = 5.4.6
-install-lua: ## Install lua
+lua-install: ## Install lua
 	sudo apt install -y build-essential libreadline-dev unzip wget
 	curl -R -O http://www.lua.org/ftp/lua-${LUA_VER}.tar.gz
 	tar -zxf lua-${LUA_VER}.tar.gz
