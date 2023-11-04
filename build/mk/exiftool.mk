cf6c35c86029e49d7ece262f7e061198b340ca23
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-11-05 09:02:23 +1100

    update

--- build/mk/exiftool.mk
+++ build/mk/exiftool.mk
@@ -1,4 +1,4 @@
-EXIF_VER = 12.62
+EXIF_VER = 12.69
 exiftool-install: # Install ExifTool
 	wget https://exiftool.org/Image-ExifTool-$(EXIF_VER).tar.gz
 	gzip -dc Image-ExifTool-$(EXIF_VER).tar.gz | tar -xf -
