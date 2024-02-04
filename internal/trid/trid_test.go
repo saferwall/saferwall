fa67db783c95639a83fac10230446660d8d2c74f
LordNoteworthy <lord.noteworthy@protonmail.com>
2024-02-04 17:33:01 +1100

    fix trid test

--- internal/trid/trid_test.go
+++ internal/trid/trid_test.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Saferwall. All rights reserved.
+// Copyright 2018 Saferwall. All rights reserved.
 // Use of this source code is governed by Apache v2 license
 // license that can be found in the LICENSE file.
 
@@ -23,7 +23,7 @@ var tridtests = []struct {
 }{
 	{getAbsoluteFilePath("testdata/putty.exe"),
 		[]string{
-			"42.7% (.EXE) Win32 Executable (generic) (4505/5/1)",
+			"42.7% (.EXE) Win32 Executable (generic) (4504/4/1)",
 			"19.2% (.EXE) OS/2 Executable (generic) (2029/13)",
 			"19.0% (.EXE) Generic Win/DOS Executable (2002/3)",
 			"18.9% (.EXE) DOS Executable Generic (2000/1)",
