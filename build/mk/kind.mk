bd2f64195da212aa35a94b52904a6b81b0d478f1
Ayoub Faouzi <ayoubfaouzi@users.noreply.github.com>
2025-10-08 11:14:51 +0100

    remove helm chart from repo

--- build/mk/kind.mk
+++ build/mk/kind.mk
@@ -1,4 +1,4 @@
-KIND_VERSION = 0.20.0
+KIND_VERSION = 0.30.0
 kind/install: ## Install Kind for local kubernetes cluster deployment.
 	curl -o kind -sS -L https://kind.sigs.k8s.io/dl/v$(KIND_VERSION)/kind-linux-amd64
 	chmod +x kind
@@ -14,7 +14,7 @@ kind-create-cluster:	## Create Kind cluster.
 kind-deploy-ingress-nginx: ## Deploy ingress-nginx in Kind.
 	# The manifests contains kind specific patches to forward the hostPorts to the ingress controller,
 	# set taint tolerations and schedule it to the custom labelled node.
-	kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+	kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
 	# Wait a bit before probing for pods, otherwise you get: error: no matching resources found
 	sleep 30s
 	# Now the Ingress is all setup. Wait until is ready to process requests running:
