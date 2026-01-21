8e9cd6d15faa5e7313930a76e6f58b739fecf1dc
Ayoub Faouzi <ayoubfaouzi@users.noreply.github.com>
2026-01-21 18:54:43 +0100

    update couchbase/init

--- build/mk/couchbase.mk
+++ build/mk/couchbase.mk
@@ -1,20 +1,8 @@
-COUCHBASE_CONTAINER_NAME = cb-db
-
-couchbase/start:	## Start Couchbase Server docker container.
-	$(eval COUCHBASE_CONTAINER_STATUS := $(shell sudo docker container inspect -f '{{.State.Status}}' $(COUCHBASE_CONTAINER_NAME)))
-ifeq ($(COUCHBASE_CONTAINER_STATUS),running)
-	@echo "All good, couchabse server is already running."
-else
-	@echo "No stress, creating one ..."
-	sudo docker run -d --name $(COUCHBASE_CONTAINER_NAME) -p 8091-8094:8091-8094 -p 11210:11210 couchbase:enterprise-6.6.0
-endif
-
 couchabse/install:	## Install Couchbase in Ubuntu
 	curl -O https://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-amd64.deb
 	sudo dpkg -i ./couchbase-release-1.0-amd64.deb
 	sudo apt-get update
 	sudo apt-get install couchbase-server -y
-
 
 couchbase/start:	## Start Couchbase Server docker container.
 	$(eval COUCHBASE_CONTAINER_STATUS := $(shell docker container inspect -f '{{.State.Status}}' $(COUCHBASE_CONTAINER_NAME)))
